62b2457b2e510aabc75b6506d8ffcb12f57f218d
RetroMike <michaeldlesk@gmail.com>
2024-09-28 13:38:41 -0500

    Update client_handler.go

--- src/kaspastratum/client_handler.go
+++ src/kaspastratum/client_handler.go
@@ -105,11 +105,11 @@ func (c *clientListener) NewBlockAvailable(kapi *KaspaApi) {
 			if err != nil {
 				if strings.Contains(err.Error(), "Could not decode address") {
 					RecordWorkerError(client.WalletAddr, ErrInvalidAddressFmt)
-					client.Logger.Error(fmt.Sprintf("failed fetching new block template from kaspa, malformed address: %s", err))
+					client.Logger.Error(fmt.Sprintf("failed fetching new block template from node, malformed address: %s", err))
 					client.Disconnect() // unrecoverable
 				} else {
 					RecordWorkerError(client.WalletAddr, ErrFailedBlockFetch)
-					client.Logger.Error(fmt.Sprintf("failed fetching new block template from kaspa: %s", err))
+					client.Logger.Error(fmt.Sprintf("failed fetching new block template from node: %s", err))
 				}
 				return
 			}
@@ -179,7 +179,7 @@ func (c *clientListener) NewBlockAvailable(kapi *KaspaApi) {
 			go func() {
 				balances, err := kapi.kaspad.GetBalancesByAddresses(addresses)
 				if err != nil {
-					c.logger.Warn("failed to get balances from kaspa, prom stats will be out of date", zap.Error(err))
+					c.logger.Warn("failed to get balances from node, prom stats will be out of date", zap.Error(err))
 					return
 				}
 				RecordBalances(balances)
